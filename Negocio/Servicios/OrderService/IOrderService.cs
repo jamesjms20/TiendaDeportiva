56c5984a2dfc9d465176ae40c2806eb10616f556
jamesjms20 <jamessoscue20@gmail.com>
2023-06-08 19:19:54 -0500

    add order service

---
+++ Negocio/Servicios/OrderService/IOrderService.cs
@@ -0,0 +1,23 @@
+﻿using Compartido.Entidades;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Negocio.Servicios.OrderService
+{
+    public interface IOrderService
+    {
+        List<Order> Orders();
+        List<Order> GetOrdersByperson(int perId);
+        Order GetById(int id);
+        Order Save(Order order);
+        Order Update(Order order);
+        bool Delete(int id);
+        bool AddProduct(int orId, int pId);
+        bool DeleteProduct(int orId, int pId);
+
+        List<Producto> GetProducts(int orId);
+    }
+}
---
+++ Negocio/Servicios/OrderService/OrderService.cs
@@ -0,0 +1,64 @@
+﻿using AccesoDatos.Repositories.OrderRepository;
+using Compartido.Entidades;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Negocio.Servicios.OrderService
+{
+    public class OrderService : IOrderService
+    {
+        private readonly IOrderRepository _orderRepository;
+
+        public OrderService(IOrderRepository orderRepository)
+        {
+            _orderRepository = orderRepository;
+        }
+        public bool AddProduct(int orId, int pId)
+        {
+            return _orderRepository.AddProduct(orId, pId);
+        }
+
+        public bool Delete(int id)
+        {
+            return _orderRepository.Delete(id);
+        }
+
+        public bool DeleteProduct(int orId, int pId)
+        {
+            return _orderRepository.DeleteProduct(orId, pId);
+        }
+
+        public Order GetById(int id)
+        {
+            return _orderRepository.GetById(id);
+        }
+
+        public List<Order> GetOrdersByperson(int perId)
+        {
+            return _orderRepository.GetOrdersByperson(perId);
+        }
+
+        public List<Producto> GetProducts(int orId)
+        {
+            return _orderRepository.GetProducts(orId);
+        }
+
+        public List<Order> Orders()
+        {
+            return _orderRepository.Orders();
+        }
+
+        public Order Save(Order order)
+        {
+            return _orderRepository.Save(order);
+        }
+
+        public Order Update(Order order)
+        {
+            return _orderRepository.Update(order);
+        }
+    }
+}
